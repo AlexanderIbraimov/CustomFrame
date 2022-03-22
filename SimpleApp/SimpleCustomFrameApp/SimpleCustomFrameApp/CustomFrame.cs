1ce71c58f89d58b3f8574bacb0c94aef9f2121f2
Alexander Ibraimov <sashaibraimov@gmail.com>
2022-03-22 09:49:21 +0600

    added simple app

---
+++ SimpleApp/SimpleCustomFrameApp/SimpleCustomFrameApp/CustomFrame.cs
@@ -0,0 +1,20 @@
+﻿using Xamarin.Forms;
+
+namespace SimpleCustomFrameApp
+{
+    public class CustomFrame : Frame
+    {
+        public CustomFrame()
+        {
+            base.CornerRadius = 0;
+        }
+
+        public new static readonly BindableProperty CornerRadiusProperty = BindableProperty.Create(nameof(CustomFrame), typeof(CornerRadius), typeof(CustomFrame));
+
+        public new CornerRadius CornerRadius
+        {
+            get => (CornerRadius)GetValue(CornerRadiusProperty);
+            set => SetValue(CornerRadiusProperty, value);
+        }
+    }
+}
