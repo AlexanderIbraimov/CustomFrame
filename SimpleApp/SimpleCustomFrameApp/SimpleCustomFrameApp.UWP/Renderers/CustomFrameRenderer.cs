0a8741186042d63b392e7bcdee3287c41009439a
Alexander Ibraimov <sashaibraimov@gmail.com>
2022-03-22 09:50:21 +0600

    added renderer for UWP

---
+++ SimpleApp/SimpleCustomFrameApp/SimpleCustomFrameApp.UWP/Renderers/CustomFrameRenderer.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.UWP;
+
+[assembly: ExportRenderer(typeof(SimpleCustomFrameApp.CustomFrame), typeof(SimpleCustomFrameApp.UWP.Renderers.CustomFrameRenderer))]
+
+namespace SimpleCustomFrameApp.UWP.Renderers
+{
+    public class CustomFrameRenderer : FrameRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Frame> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.NewElement != null && Control != null)
+            {
+                UpdateCornerRadius();
+            }
+        }
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+
+            if (e.PropertyName == nameof(CustomFrame.CornerRadius) ||
+                e.PropertyName == nameof(CustomFrame))
+            {
+                UpdateCornerRadius();
+            }
+        }
+
+        private void UpdateCornerRadius()
+        {
+            var radius = ((CustomFrame)this.Element).CornerRadius;
+            Control.CornerRadius = new Windows.UI.Xaml.CornerRadius(radius.TopLeft, radius.TopRight, radius.BottomRight, radius.BottomLeft);
+        }
+    }
+}
